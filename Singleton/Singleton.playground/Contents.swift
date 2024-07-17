8c3a99946a166fcf9c454a34321c996596897700
Akash Sen <53941291+2207akash@users.noreply.github.com>
2024-07-17 23:31:43 +0530

    Update Contents.swift
    Simplified implementation of Singleton design pattern implementation in Swift

--- Singleton/Singleton.playground/Contents.swift
+++ Singleton/Singleton.playground/Contents.swift
@@ -2,35 +2,13 @@
  
     Author: Akash Sen
     Singleton Design Pattern
-    (With double lock check and handling multi-threading environment)
  */
 
 import Foundation
 
-class Singleton {
-    
-    private static var shared: Singleton!
-    private static let semaphore = DispatchSemaphore(value: 1)  // To handle case for multithreading
-    
-    static var count = 0
-    
-    // MARK: Private methods
-    private init() {
-        Singleton.count += 1
-        print("Object count = \(Singleton.count)")
-    }
-    
-    // MARK: Public methods
-    static func getInstance() -> Singleton {
-        if shared == nil {  // No need to put lock if instance is already created
-            semaphore.wait()    // To avoid multiple threads to access critical section
-            if shared == nil {
-                shared = Singleton()
-            }
-            semaphore.signal()
-        }
-        return shared
-    }
+class Singleton { 
+    static let shared = Singleton()
+    private init() {}
 }
 
 
@@ -38,9 +16,10 @@ let concurrentGroup = DispatchGroup()
 let concurrentQueue = DispatchQueue(label: "concurrent.access.singleton", attributes: .concurrent)
 
 concurrentQueue.async(group: concurrentGroup) {
-    let singletonObject1 = Singleton.getInstance()
+    let singletonObject1 = Singleton.shared
 }
 
 concurrentQueue.async(group: concurrentGroup) {
-    let singletonObject2 = Singleton.getInstance()
+    let singletonObject2 = Singleton.shared
 }
+
